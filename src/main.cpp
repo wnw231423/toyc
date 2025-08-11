02ed9670d0d0569a2dfca689dbfedfcab93f896d
lkyyy255 <14640500+lkyyy255@user.noreply.gitee.com>
2025-08-11 14:20:15 +0800

    opt

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <cassert>
 #include <cstdio>
+#include <cstring>
 #include <iostream>
 #include <memory>
 #include <string>
@@ -25,49 +26,9 @@ extern int yyparse(unique_ptr<BaseAST> &ast);
  */
 int main(int argc, char *argv[])
 {
-    int ast_mode = 0;
-    int ir_mode = 0;
-    int inline_mode = 0;
-    int inline_asm_mode = 0;
-    int consprop_mode = 0;
-    int consprop_asm_mode = 0;
-    char *input;
+    int opt = 0;
 
-    assert(argc == 2 || argc == 3);
-    if (argc == 3)
-    {
-        if (string(argv[1]) == "-a")
-        {
-            ast_mode = 1;
-        }
-        else if (string(argv[1]) == "-ir")
-        {
-            ir_mode = 1;
-        }
-        else if (string(argv[1]) == "-inline")
-        {
-            inline_mode = 1;
-        }
-        else if (string(argv[1]) == "-inline-asm")
-        {
-            inline_asm_mode = 1;
-        }
-        else if (string(argv[1]) == "-consprop")
-        {
-            consprop_mode = 1;
-        }
-        else if (string(argv[1]) == "-consprop-asm")
-        {
-            consprop_asm_mode = 1;
-        }
-        input = argv[2];
-    }
-    else
-    {
-        input = argv[1];
-    }
-
-    yyin = fopen(input, "r");
+    yyin = stdin;
     assert(yyin);
 
     unique_ptr<BaseAST> ast;
@@ -75,58 +36,28 @@ int main(int argc, char *argv[])
     assert(!ret);
 
     auto comp_unit = dynamic_cast<CompUnitAST *>(ast.get());
-    if (ast_mode)
+
+    for (int i = 0; i < argc; ++i)
     {
-        comp_unit->Dump(0);
+        if (strcmp(argv[i], "-opt") == 0)
+        {
+            opt = 1;
+            // opt = 0;
+        }
     }
-    else if (ir_mode)
-    {
-        cout << comp_unit->to_IR()->toString() << endl;
-    }
-    else if (inline_mode)
+
+    if (opt)
     {
         auto program = comp_unit->to_IR();
-
-        // 执行函数内联优化
-        InlineOptimizer optimizer(3, 50); // 深度限制3，大小限制50
-        optimizer.optimize(program.get());
-
-        cout << "// 优化后的IR代码:" << endl;
-        cout << program->toString() << endl;
-    }
-    else if (inline_asm_mode)
-    {
-        auto program = comp_unit->to_IR();
-
-        // 执行函数内联优化
-        InlineOptimizer optimizer(3, 50); // 深度限制3，大小限制50
-        optimizer.optimize(program.get());
-
-        cout << "// 内联优化后的汇编代码:" << endl;
-        cout << visit_program(std::move(program)) << endl;
-    }
-    else if (consprop_mode)
-    {
-        auto program = comp_unit->to_IR();
+        // InlineOptimizer optimizer(3, 50);
+        // optimizer.optimize(program.get());
         ConstantPropagationOptimizer optimizer;
         optimizer.optimize(program.get());
-
-        cout << "// 常量传播+死代码消除优化后的IR: " << endl;
-        cout << program->toString() << endl;
-    }
-    else if (consprop_asm_mode)
-    {
-        auto program = comp_unit->to_IR();
-        ConstantPropagationOptimizer optimizer;
-        optimizer.optimize(program.get());
-
-        cout << "// 常量传播+死代码消除后的汇编代码: " << endl;
         cout << visit_program(std::move(program)) << endl;
     }
     else
     {
         cout << visit_program(comp_unit->to_IR()) << endl;
     }
-
     return 0;
 }
